076a7a4b31d0915221b5527fda673dc9aea6faa6
Roeich <roeichsheikh@gmail.com>
2025-04-21 21:56:14 +0600

    added images and sms functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ const swiper = new Swiper('.hero-swiper', {
     },
     loop: true,
     autoplay: {
-        delay: 5000,
+        delay: 2000,
         disableOnInteraction: false,
     },
     speed: 1000,
